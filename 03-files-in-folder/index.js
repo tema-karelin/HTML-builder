b4fc595bf11697b3cc6a2814690b02d63f4b92fa
Artem Karelin <Artem.Karelin@ibm.com>
2022-05-23 22:41:04 +0300

    feat: 03-files-in-folder task os resolved (Mon May 23 2022 22:40:38 GMT+0300)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+
+const targetDir = path.join(__dirname, 'secret-folder');
+
+console.log("\nList of files in secret-folder directory:\n");
+
+async function readDirectory(dir) {
+  try {
+    const files = await readdir(dir, {withFileTypes: true});
+    for (const file of files) {
+      if (file.isFile()) {
+        let extension  = path.extname(file.name);
+        // let line = '               -     - ';
+        let line = '';
+        const fileName = file.name.slice(0, file.name.indexOf(extension));
+        extension = extension.slice(1);
+        // line = fileName + line.slice(fileName.length) + extension;
+        line = fileName + ' - ' + extension + ' - ';
+        const filePath = path.join(dir, file.name);
+        fs.stat(filePath, (err, stats) => {
+          line = line + (stats.size/1024).toFixed(3).toString() + 'kb';
+          console.log(line);
+        });
+      }
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+readDirectory(targetDir);
+
+
+
